bb94ebd8d8629dd3245e9637bba4a78f8fbbe1e2
Ethan Bellem <ethan.bellem@ocsbstudent.ca>
2018-03-20 14:35:06 -0400

    Finished off thecode

--- main.lua
+++ main.lua
@@ -16,7 +16,39 @@ local button = display.newImageRect( "./assets/sprite/enterButton.png", 550, 300
 button.x = 1500
 button.y = 500
 
-local text = native.newTextField( 600, 500, 750, 100)
+local numberEntered
+
+local textone = native.newTextField( 600, 500, 750, 100)
 
 local instructions = display.newText( "Enter An Integer", display.contentCenterX, 200, native.systemFont, 100)
 instructions:setTextColor(1, 1, 1)
+
+-- now i will make a function to tell me wheter or not my number is positive or negative
+local function onClick( event )
+	numberEntered = tonumber(textone.text)
+
+
+	if numberEntered > 0 then 
+		instructions.alpha = 0
+		button.alpha = 0
+		local positiveText = display.newText( "Your Number is Positive", display.contentCenterX, display.contentCenterY, native.systemFont, 125)
+		positiveText:setTextColor( 0, 1, 0 )
+	else 
+		if numberEntered < 0 then
+
+		    instructions.alpha = 0
+		    button.alpha = 0
+		    local negativeText = display.newText("Your Number is Negative",display.contentCenterX, display.contentCenterY, native.systemFont, 125)
+		    negativeText:setTextColor( 1, 0, 0 )
+	    else
+		    instructions.alpha = 0
+		    button.alpha = 0
+		    local zeroText = display.newText("Invalid Number",display.contentCenterX, display.contentCenterY, native.systemFont, 125)
+		    zeroText:setTextColor( 0, 0, 1 )
+		end    
+	end
+
+end
+
+
+button:addEventListener("touch", onClick)
